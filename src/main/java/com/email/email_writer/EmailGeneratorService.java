0181b49dd47b3d6fac3abdd2f0281195acc1d7d8
nadunrandeera <nadunrandeera@gmail.com>
2025-10-17 13:58:49 +0530

    Edit configurs

--- src/main/java/com/email/email_writer/EmailGeneratorService.java
+++ src/main/java/com/email/email_writer/EmailGeneratorService.java
@@ -1,19 +1,72 @@
 package com.email.email_writer;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class EmailGeneratorService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
     public String generateEmailReply(EmailRequest emailRequest){
         String prompt = buildpromt(emailRequest);
+        Map<String,Object> requestBody = Map.of(
+                "contents",new Object[]{
+                        Map.of("parts",new Object[]{
+                                Map.of("text", prompt),
+                        })
+                }
+        );
+
+        String response = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block(); 
+        
+        return extractResponseContent(response);
     }
+
+    private String extractResponseContent(String response) {
+         try{
+             ObjectMapper mapper = new ObjectMapper();
+             JsonNode rootNode = mapper.readTree(response);
+             return rootNode.path("candidate")
+                     .get(0)
+                     .path("content")
+                     .path("parts")
+                     .get(0)
+                     .path("text")
+                     .asText();
+         }catch (Exception e){
+             return "Error processing request: " + e.getMessage();
+         }
+    }
+
     private String buildpromt(EmailRequest emailRequest){
         StringBuilder prompt = new StringBuilder();
         prompt.append("Generate a professional email from the following form:\n");
         if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
             prompt.append("Tone").append(emailRequest.getTone()).append("\n");
         }
-        prompt.append("\noriginal email\n").append(emailRequest.getEmailcontent());
+        prompt.append("\noriginal email\n").append(emailRequest.getEmailContent());
         return prompt.toString();
     }
 
